7347c0eab19a3038736cffb5814077e5ae9aa74f
uthamprabhu <uthamprabhu1@gmail.com>
2025-01-06 15:55:40 +0530

    users.js helper fn impl full

--- backend/users.js
+++ backend/users.js
@@ -17,14 +17,16 @@ const addUser = ({ id, name, room }) => {
     return { user }
 }
 
-const removeUser = () => {
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
 
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
 }
 
-const getUser = () => {
+const getUser = (id) => users.find((user) => user.id === id)
 
-}
+const getUsersInRoom = (room) => users.filter((user) => user.room === room)
 
-const getUsersInRoom = () => {
-
-}
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
